0356abf31723a267ec66c802ad7b19bcb00d0a82
Jonnathan-Landi <jonnathana.landi@ucuenca.edu.ec>
2025-03-31 21:03:40 -0500

    Development tests for algorithms

--- tests/testthat/test-IDW.R
+++ tests/testthat/test-IDW.R
@@ -6,10 +6,11 @@ test_that("IDW interpolation works correctly", {
   shapefile <- terra::vect(system.file("extdata/study_area.shp", package = "InterpolateR"))
 
   # Performing interpolation using the IDW method
-  Interpolated_data <- IDW(BD_Obs, BD_Coord, shapefile, resolution = 5, p = 2, n_round = 1)
+  Interpolated_data <- IDW(BD_Obs, BD_Coord, shapefile, grid_resolution = 5, p = 2,
+                           n_round = 1, training = 0.8, Rain_threshold = NULL,
+                           stat_validation = NULL, save_model = FALSE, name_save = NULL)
 
   # Check that the result is a raster object
-  expect_true(inherits(Interpolated_data, "SpatRaster"))
-  expect_equal(terra::nlyr(Interpolated_data), length(unique(BD_Obs$Date)))
-
+  expect_true(inherits(Interpolated_data$Ensamble, "SpatRaster"))
+  expect_equal(terra::nlyr(Interpolated_data$Ensamble), length(unique(BD_Obs$Date)))
 })
---
+++ tests/testthat/test-RFmerge.R
@@ -0,0 +1,25 @@
+test_that("RFmerge works correctly", {
+  if (Sys.getenv("NOT_CRAN") != "true") {  # Desactiva este test en CRAN
+    data("BD_Obs", package = "InterpolateR")
+    data("BD_Coord", package = "InterpolateR")
+
+    # Load the covariates
+    cov <- list(
+      MSWEP = terra::rast(system.file("extdata/MSWEP.nc", package = "InterpolateR")),
+      CHIRPS = terra::rast(system.file("extdata/CHIRPS.nc", package = "InterpolateR")),
+      DEM = terra::rast(system.file("extdata/DEM.nc", package = "InterpolateR"))
+    )
+
+    # Apply the RFmerge
+    model_RFmerge = RFmerge(BD_Obs, BD_Coord, cov, mask = NULL, n_round = 1, ntree = 2000,
+                            seed = 123,  training = 1, stat_validation = c("M006"), Rain_threshold = NULL,
+                            save_model = FALSE, name_save = NULL)
+
+    # Check that the result is a raster object
+    expect_true(inherits(model_RFmerge$Ensamble, "SpatRaster"))
+    expect_equal(terra::nlyr(model_RFmerge$Ensamble), length(unique(BD_Obs$Date)))
+  } else {
+    # Si estás en CRAN, omite esta prueba.
+    message("Skipping test on CRAN.")
+  }
+})
