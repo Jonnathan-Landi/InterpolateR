221f524a26eb490271d14e95eef4fb327a2ea001
Jonnathan-Landi <jonnathana.landi@ucuenca.edu.ec>
2025-04-02 22:41:14 -0500

    filling in test gaps

--- tests/testthat/test-RFplus.R
+++ tests/testthat/test-RFplus.R
@@ -20,14 +20,33 @@ test_that("RFplus works correctly", {
     extremely_rain= c(40, Inf)
   )
 
-  # Apply the RFplus
-  model = RFplus(BD_Obs, BD_Coord, Covariates, n_round = 1, wet.day = 0.1,
+  # Apply the RFplus with method = "none"
+  model_none = RFplus(BD_Obs, BD_Coord, Covariates, n_round = 1, wet.day = 0.1,
                  ntree = 2000, seed = 123, training = 1, stat_validation = c("M006"),
                  Rain_threshold = Rain_threshold, method = "none",
                  ratio = 15, save_model = FALSE, name_save = NULL)
 
+  # Apply the method RQUANT
+  model_Rquant = RFplus(BD_Obs, BD_Coord, Covariates, n_round = 1, wet.day = 0.1,
+                 ntree = 2000, seed = 123, training = 1, stat_validation = c("M006"),
+                 Rain_threshold = Rain_threshold, method = "RQUANT",
+                 ratio = 15, save_model = FALSE, name_save = NULL)
+
+  # Apply the method QUANT
+  model_QUANT = RFplus(BD_Obs, BD_Coord, Covariates, n_round = 1, wet.day = 0.1,
+                 ntree = 2000, seed = 123, training = 1, stat_validation = c("M006"),
+                 Rain_threshold = Rain_threshold, method = "QUANT",
+                 ratio = 15, save_model = FALSE, name_save = NULL)
+
+  ##################################################################################################
   # Check that the result is a raster object
-  expect_true(inherits(model$Ensamble, "SpatRaster"))
-  expect_equal(terra::nlyr(model$Ensamble), length(unique(BD_Obs$Date)))
+  expect_true(inherits(model_none$Ensamble, "SpatRaster"))
+  expect_true(inherits(model_Rquant$Ensamble, "SpatRaster"))
+  expect_true(inherits(model_QUANT$Ensamble, "SpatRaster"))
+
+  # Check that the number of layers in the raster object is equal to the number of unique dates
+  expect_equal(terra::nlyr(model_none$Ensamble), length(unique(BD_Obs$Date)))
+  expect_equal(terra::nlyr(model_Rquant$Ensamble), length(unique(BD_Obs$Date)))
+  expect_equal(terra::nlyr(model_QUANT$Ensamble), length(unique(BD_Obs$Date)))
 })
 
