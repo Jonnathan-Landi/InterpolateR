0cab547d7d4d5e917b90094c99c28c51acb50c41
Jonnathan-Landi <jonnathana.landi@ucuenca.edu.ec>
2025-04-08 16:31:51 -0500

    update test

--- tests/testthat/test-RFplus.R
+++ tests/testthat/test-RFplus.R
@@ -59,5 +59,148 @@ test_that("RFplus works correctly", {
   expect_true(inherits(model_sn, "SpatRaster"))
   # Check that the number of layers in the raster object is equal to the number of unique dates
   expect_equal(terra::nlyr(model_sn), length(unique(BD_Obs$Date)))
+
+  ##############################################################################
+  #     Check that the algorithm stops when the input data is not correct.     #
+  ##############################################################################
+  # Verify that covariates is a list
+  Cov_df <- data.frame(x = 1:10, y = rnorm(10))
+  resultado <- tryCatch({
+    RFplus(BD_Obs, BD_Coord, Cov_df, n_round = 1, wet.day = 0.1,
+           ntree = 2000, seed = 123, training = 1, stat_validation = NULL,
+           Rain_threshold = NULL, method = "none",
+           ratio = 15, save_model = FALSE, name_save = NULL)
+  }, error = function(e) {
+    message("Parameter detected correctly: ", e$message)
+    return(NULL)
+  }, warning = function(w) {
+    message("warning: ", w$message)
+    return(NULL)
+  })
+
+  # Verify that the covariates are type SpatRaster
+  cov = list(
+    MSWEP = terra::rast(system.file("extdata/MSWEP.nc", package = "InterpolateR")),
+    CHIRPS = terra::rast(system.file("extdata/CHIRPS.nc", package = "InterpolateR")),
+    DEM = data.frame(x = 1:10, y = rnorm(10))
+  )
+
+  resultado <- tryCatch({
+    RFplus(BD_Obs, BD_Coord, cov, n_round = 1, wet.day = 0.1,
+           ntree = 2000, seed = 123, training = 1, stat_validation = NULL,
+           Rain_threshold = NULL, method = "none",
+           ratio = 15, save_model = FALSE, name_save = NULL)
+  }, error = function(e) {
+    message("Parameter detected correctly: ", e$message)
+    return(NULL)
+  }, warning = function(w) {
+    message("warning: ", w$message)
+    return(NULL)
+  })
+
+  # Verify the BD_Obs is data.table
+  BD = as.matrix(BD_Obs)
+  cord = as.matrix(BD_Coord)
+
+  resultado <- tryCatch({
+    RFplus(BD, BD_Coord, Covariates, n_round = 1, wet.day = 0.1,
+           ntree = 2000, seed = 123, training = 1, stat_validation = NULL,
+           Rain_threshold = NULL, method = "none",
+           ratio = 15, save_model = FALSE, name_save = NULL)
+  }, error = function(e) {
+    message("Parameter detected correctly: ", e$message)
+    return(NULL)
+  }, warning = function(w) {
+    message("warning: ", w$message)
+    return(NULL)
+  })
+
+  # Verify the BD_Obs is data.table
+  resultado <- tryCatch({
+    RFplus(BD_Obs, cord, Covariates, n_round = 1, wet.day = 0.1,
+           ntree = 2000, seed = 123, training = 1, stat_validation = NULL,
+           Rain_threshold = NULL, method = "none",
+           ratio = 15, save_model = FALSE, name_save = NULL)
+  }, error = function(e) {
+    message("Parameter detected correctly: ", e$message)
+    return(NULL)
+  }, warning = function(w) {
+    message("warning: ", w$message)
+    return(NULL)
+  })
+
+  # Verify that all dates have at least one entry recorded
+  BD_na = BD_Obs
+  BD_na[1, 2:11] <- NA
+  resultado <- tryCatch({
+    RFplus(BD_na, BD_Coord, Covariates, n_round = 1, wet.day = 0.1,
+           ntree = 2000, seed = 123, training = 1, stat_validation = NULL,
+           Rain_threshold = NULL, method = "none",
+           ratio = 15, save_model = FALSE, name_save = NULL)
+  }, error = function(e) {
+    message("Parameter detected correctly: ", e$message)
+    return(NULL)
+  }, warning = function(w) {
+    message("warning: ", w$message)
+    return(NULL)
+  })
+
+  # Check that the coordinate names appear in the observed data
+  bd_2 = BD_Coord
+  bd_2[3,1] <- "aa"
+  resultado <- tryCatch({
+    RFplus(BD_Obs, bd_2, Covariates, n_round = 1, wet.day = 0.1,
+           ntree = 2000, seed = 123, training = 1, stat_validation = NULL,
+           Rain_threshold = NULL, method = "none",
+           ratio = 15, save_model = FALSE, name_save = NULL)
+  }, error = function(e) {
+    message("Parameter detected correctly: ", e$message)
+    return(NULL)
+  }, warning = function(w) {
+    message("warning: ", w$message)
+    return(NULL)
+  })
+
+  # Check if there is a DEM layer
+  COV_2 = list(
+    MSWEP = terra::rast(system.file("extdata/MSWEP.nc", package = "InterpolateR")),
+    CHIRPS = terra::rast(system.file("extdata/CHIRPS.nc", package = "InterpolateR")),
+    DEM = rep(terra::rast(system.file("extdata/DEM.nc", package = "InterpolateR")),2)
+  )
+
+  resultado <- tryCatch({
+    RFplus(BD_Obs, BD_Coord, COV_2, n_round = 1, wet.day = 0.1,
+           ntree = 2000, seed = 123, training = 1, stat_validation = NULL,
+           Rain_threshold = NULL, method = "none",
+           ratio = 15, save_model = FALSE, name_save = NULL)
+  }, error = function(e) {
+    message("Parameter detected correctly: ", e$message)
+    return(NULL)
+  }, warning = function(w) {
+    message("warning: ", w$message)
+    return(NULL)
+  })
+
+  # Verify the layers of the covariates.
+  Cov_3 = list(
+    MSWEP = rep(terra::rast(system.file("extdata/MSWEP.nc", package = "InterpolateR")),2),
+    CHIRPS = terra::rast(system.file("extdata/CHIRPS.nc", package = "InterpolateR")),
+    DEM = terra::rast(system.file("extdata/DEM.nc", package = "InterpolateR"))
+  )
+
+  resultado <- tryCatch({
+    RFplus(BD_Obs, BD_Coord, Cov_3, n_round = 1, wet.day = 0.1,
+           ntree = 2000, seed = 123, training = 1, stat_validation = NULL,
+           Rain_threshold = NULL, method = "none",
+           ratio = 15, save_model = FALSE, name_save = NULL)
+  }, error = function(e) {
+    message("Parameter detected correctly: ", e$message)
+    return(NULL)
+  }, warning = function(w) {
+    message("warning: ", w$message)
+    return(NULL)
+  })
+
+
 })
 
