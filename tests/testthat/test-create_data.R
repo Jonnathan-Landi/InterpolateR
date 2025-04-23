951c954f6c2b2ac8eb7581f814f68ef98270e731
Jonnathan-Landi <jonnathana.landi@ucuenca.edu.ec>
2025-04-22 22:02:27 -0500

    add test for create_data

---
+++ tests/testthat/test-create_data.R
@@ -0,0 +1,19 @@
+test_that("Create data Method works correctly", {
+  skip_on_cran()
+  file.path = system.file("extdata/Folds_ejs_create_data", package = "InterpolateR")
+
+  # Test with default parameters
+  data = create_data(file.path, Start_date = "2015-01-01", End_Date = "2015-03-01", ncores = 1)
+  print(head(data))
+
+  # Test with different parameters
+  data_2 = create_data(file.path, Start_date = "2015-01-01", End_Date = "2015-03-01",
+                       ncores = 1, max.na = 10)
+
+  # Visualize the data
+  print(head(data_2$data))
+
+  # Visualize percentage of missing data
+  print(head(data_2$Na_stations))
+
+}
