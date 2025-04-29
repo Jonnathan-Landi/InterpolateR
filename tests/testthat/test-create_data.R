32e25ad9625da5cf51cfc8d5f192b6c5809f31b1
Jonnathan-Landi <jonnathana.landi@ucuenca.edu.ec>
2025-04-29 13:00:10 -0500

    Create data test updated to the new format

--- tests/testthat/test-create_data.R
+++ tests/testthat/test-create_data.R
@@ -1,19 +1,33 @@
-testthat::test_that("Create data Method works correctly", {
-  skip_on_cran()
-  file.path <- system.file("extdata/Folds_ejs_create_data", package = "InterpolateR")
+# Data used for create data
+# Date of test creation: 2025-04-29
+# Test update date: 2025-04-29
 
-  # Test with default parameters
-  data <- create_data(file.path,
+file.path <- system.file("extdata/Folds_ejs_create_data", package = "InterpolateR")
+skip_on_cran()
+# 1. Thest with default parameters
+testthat::test_that("Create data Method with basic input", {
+  out  <- create_data(file.path,
     Start_date = "2015-01-01", End_Date = "2015-03-01",
-    ncores = NULL
+    ncores = NULL, max.na = NULL
   )
+  testthat::expect_true(inherits(out, "data.table"))
+})
 
-  # Test with different parameters
-  data_2 <- create_data(file.path,
-    Start_date = "2015-01-01", End_Date = "2015-03-01",
-    ncores = NULL, max.na = 10
+# 2. Test with different parameters (max.na set to 10)
+testthat::test_that("Create data Method with 10% NA.", {
+  out  <- create_data(file.path,
+                      Start_date = "2015-01-01", End_Date = "2015-03-01",
+                      ncores = NULL, max.na = 10
   )
+  testthat::expect_true(inherits(out$data, "data.table"))
+  testthat::expect_true(inherits(out$Na_stations, "data.table"))
+})
 
-  testthat::expect_true(inherits(data, "data.table"))
-  testthat::expect_true(inherits(data_2, "list"))
+# 3. Test with paralel processing
+testthat::test_that("Create data Method with parallel processing", {
+  out  <- create_data(file.path,
+                      Start_date = "2015-01-01", End_Date = "2015-03-01",
+                      ncores = 2, max.na = NULL
+  )
+  testthat::expect_true(inherits(out, "data.table"))
 })
