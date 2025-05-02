1c67cbe1edb0b11569dd39dcbd3af0572652ee62
Jonnathan-Landi <jonnathana.landi@ucuenca.edu.ec>
2025-05-02 13:01:12 -0500

    update test

--- tests/testthat/test-RFplus.R
+++ tests/testthat/test-RFplus.R
@@ -25,6 +25,7 @@ testthat::skip_on_cran()
 
 # 1. Testing with validation ---------------------------------------------------
 testthat::test_that("RFplus returns SpatRaster without validation.", {
+  testthat::skip_on_cran()
   out <- RFplus(BD_Obs, BD_Coord, Covariates, n_round = 1, ntree = 2000,
     seed = 123, method = "none", ratio = 10, training = 1, stat_validation = NULL,
     Rain_threshold = NULL, save_model = FALSE, name_save = NULL)
@@ -35,6 +36,7 @@ testthat::test_that("RFplus returns SpatRaster without validation.", {
 
 # 2. Testing with validation (random validation) --------------------------------
 testthat::test_that("RFplus returns SpatRaster with random validation.", {
+  testthat::skip_on_cran()
   out <- RFplus(BD_Obs, BD_Coord, Covariates,  n_round = 1, ntree = 2000, wet.day = 0.1,
     seed = 123, training = 0.8, method = "none", ratio = 10, stat_validation = NULL,
     Rain_threshold = Rain_threshold, save_model = FALSE, name_save = NULL)
@@ -47,6 +49,7 @@ testthat::test_that("RFplus returns SpatRaster with random validation.", {
 
 # 3. Testing with validation (manual validation) --------------------------------
 testthat::test_that("RFplus returns SpatRaster with manual validation.", {
+  testthat::skip_on_cran()
   out <- RFplus(BD_Obs, BD_Coord, Covariates,  n_round = 1, ntree = 2000,
     seed = 123, training = 1, method = "none", ratio = 10, stat_validation = "M004",
     Rain_threshold = Rain_threshold, save_model = FALSE, name_save = NULL)
@@ -59,6 +62,7 @@ testthat::test_that("RFplus returns SpatRaster with manual validation.", {
 
 # 4. Testing  RFplus returns SpatRaster with random validation without ---------
 testthat::test_that("RFplus returns SpatRaster with manual validation.", {
+  testthat::skip_on_cran()
   out <- RFplus(BD_Obs, BD_Coord, Covariates,  n_round = 1, ntree = 2000,
     seed = 123, training = 1, method = "none", ratio = 10, stat_validation = "M004",
     Rain_threshold = NULL, save_model = FALSE, name_save = NULL)
@@ -70,6 +74,7 @@ testthat::test_that("RFplus returns SpatRaster with manual validation.", {
 
 # 5. Testing  RFplus with method QUANT ---------
 testthat::test_that("RFplus uses the QUANT Method.", {
+  testthat::skip_on_cran()
   out <- RFplus(BD_Obs, BD_Coord, Covariates,  n_round = 1, ntree = 2000,
     seed = 123, training = 1, method = "QUANT", ratio = 10, stat_validation = NULL,
     Rain_threshold = NULL, save_model = FALSE, name_save = NULL)
@@ -80,6 +85,7 @@ testthat::test_that("RFplus uses the QUANT Method.", {
 
 # 6. Testing  RFplus with method RQUANT ---------
 testthat::test_that("RFplus uses the QUANT Method.", {
+  testthat::skip_on_cran()
   out <- RFplus(BD_Obs, BD_Coord, Covariates,  n_round = 1, ntree = 2000,
     seed = 123, training = 1, method = "RQUANT", ratio = 10, stat_validation = NULL,
     Rain_threshold = NULL, save_model = FALSE, name_save = NULL)
@@ -92,6 +98,7 @@ testthat::test_that("RFplus uses the QUANT Method.", {
 ##############################################################################
 # 5. Testing error with The Covariates must be a list.
 testthat::test_that("The Covariates must be a list.", {
+  testthat::skip_on_cran()
   bad_Covariates <- data.frame(x = 1:10, y = rnorm(10))
   testthat::expect_error(
     RFplus(BD_Obs, BD_Coord, bad_Covariates,  n_round = 1, ntree = 2000,
@@ -103,6 +110,7 @@ testthat::test_that("The Covariates must be a list.", {
 
 # 6. The Covariates must be of type SpatRaster.
 testthat::test_that("The Covariates must be of type SpatRaster.", {
+  testthat::skip_on_cran()
   bad_Covariates <- Covariates
   bad_Covariates$Test <- data.frame(x = 1:10, y = rnorm(10))
   testthat::expect_error(
@@ -115,6 +123,7 @@ testthat::test_that("The Covariates must be of type SpatRaster.", {
 
 # 7. The extension of the Covariates are different (all extensions should be similar)
 testthat::test_that("The extension of the Covariates are different.", {
+  testthat::skip_on_cran()
   bad_Covariates <- Covariates
   terra::ext(bad_Covariates$MSWEP) = terra::ext(bad_Covariates$MSWEP) * 2
   testthat::expect_error(
@@ -127,6 +136,7 @@ testthat::test_that("The extension of the Covariates are different.", {
 
 # 8. "The crs of the Covariates are different (all crs should be similar).
 testthat::test_that("The crs of the Covariates are different.", {
+  testthat::skip_on_cran()
   bad_Covariates <- Covariates
   terra::crs(bad_Covariates$MSWEP) = "EPSG:4326"
   testthat::expect_error(
@@ -139,6 +149,7 @@ testthat::test_that("The crs of the Covariates are different.", {
 
 # 9. "BD_Obs must be a 'data.table' or a 'data.frame'."
 testthat::test_that("BD_Obs must be a 'data.table' or a 'data.frame.", {
+  testthat::skip_on_cran()
   bad_BDObs = as.matrix(BD_Obs)
   testthat::expect_error(
     RFplus(bad_BDObs, BD_Coord, Covariates,  n_round = 1, ntree = 2000,
@@ -150,6 +161,7 @@ testthat::test_that("BD_Obs must be a 'data.table' or a 'data.frame.", {
 
 # 10. BD_Coord must be a 'data.table' or a 'data.frame'."
 testthat::test_that("BD_Coord must be a 'data.table' or a 'data.frame'.", {
+  testthat::skip_on_cran()
   bad_BD_Coord = as.matrix(BD_Coord)
   testthat::expect_error(
     RFplus(BD_Obs, bad_BD_Coord, Covariates,  n_round = 1, ntree = 2000,
@@ -161,6 +173,7 @@ testthat::test_that("BD_Coord must be a 'data.table' or a 'data.frame'.", {
 
 # 11. "The names of the coordinates do not appear in the observed data."
 testthat::test_that("names of the coordinates do not appear in the observed data.", {
+  testthat::skip_on_cran()
   bad_Cords = BD_Coord
   bad_Cords[3,1] = "yy"
   testthat::expect_error(
@@ -173,6 +186,7 @@ testthat::test_that("names of the coordinates do not appear in the observed data
 
 # 12. A single layer covariate was not found. Possibly the DEM was not entered..
 testthat::test_that("Single layer covariate was not found.", {
+  testthat::skip_on_cran()
   bad_Covs = Covariates
   bad_Covs$DEM = c(bad_Covs$DEM, bad_Covs$DEM)
   testthat::expect_error(
@@ -185,6 +199,7 @@ testthat::test_that("Single layer covariate was not found.", {
 
 # 13. Verify that all dates have at least one entry recorded
 testthat::test_that("Error when BD_Obs has rows with all NA values (except Date)", {
+  testthat::skip_on_cran()
   # Simular BD_Obs con una fila completamente NA (menos la fecha)
   BD_Obs <- data.table::data.table(
     Date = as.Date(c("2020-01-01", "2020-01-02")),
@@ -207,6 +222,7 @@ testthat::test_that("Error when BD_Obs has rows with all NA values (except Date)
 
 # 14. "Save the model must be a logical value."
 testthat::test_that("RFplus saves model when save_model = TRUE", {
+  testthat::skip_on_cran()
   temp_dir <- tempdir()
   withr::local_dir(temp_dir)
   custom_name <- "test_model_RFplus"
@@ -224,6 +240,7 @@ testthat::test_that("RFplus saves model when save_model = TRUE", {
 
 # 15. "Save the model must be a logical value." (default name) " ---------------
 testthat::test_that("RFplus saves model when save_model = TRUE (default name)", {
+  testthat::skip_on_cran()
   temp_dir <- tempdir()
   withr::local_dir(temp_dir)
   expect_message(
@@ -240,6 +257,7 @@ testthat::test_that("RFplus saves model when save_model = TRUE (default name)", 
 
 # 16. Test adicional para modulo de conexion
 testthat::test_that("The training parameter must be between 0 and 1.", {
+  testthat::skip_on_cran()
   testthat::expect_error(
     RFplus(BD_Obs, BD_Coord, Covariates, n_round = 1, ntree = 2000,
            seed = 123, training = 2, stat_validation = NULL,
